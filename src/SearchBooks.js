1116e4ceb187e0cf0aa97bc792063564ac04e9ad
Michael Sell <michael.sell@usa.net>
2020-02-22 16:52:09 +0100

    Finished Routing

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,12 +1,12 @@
-import * as BooksAPI from "./BooksAPI";
-import Book from './Book'
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import "./App.css";
+import * as BooksAPI from './BooksAPI';
+import Book from './Book';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './App.css';
 
 class SearchBooks extends Component {
   state = {
-    query: "",
+    query: '',
     mySearchList: []
   };
 
@@ -24,37 +24,30 @@ class SearchBooks extends Component {
   }
 
   render() {
-
-   const {mySearchList} = this.state;
-   console.log(mySearchList);
+    const { mySearchList } = this.state;
+    console.log(mySearchList);
 
     return (
-      <div className="search-books">
-        <div className="search-books-bar">
-
-          <Link to="/">
-            <button className="close-search"></button>
+      <div className='search-books'>
+        <div className='search-books-bar'>
+          <Link to='/'>
+            <button className='close-search'></button>
           </Link>
 
-          <div className="search-books-input-wrapper">
+          <div className='search-books-input-wrapper'>
             <input
-              type="text"
-              placeholder="Search for Books"
+              type='text'
+              placeholder='Search for Books'
               value={this.state.query}
               onChange={event => this.updateQuery(event.target.value)}
             />
           </div>
         </div>
         <div className='search-book-results'>
-           <ol className='books-grid'>
-           {
-              mySearchList && mySearchList.map( book => (
-               <Book key={book.key} book={book}/>
-              ))    
-         }
-           </ol>
-         
-
+          <ol className='books-grid'>
+            {mySearchList &&
+              mySearchList.map(book => <Book key={book.key} book={book} />)}
+          </ol>
         </div>
       </div>
     );
