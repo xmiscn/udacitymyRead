8fb8a9dfcf7934710ffafba6f3d6a2fa565fa077
Michael Sell <michael.sell@usa.net>
2020-02-22 17:02:32 +0100

    prep. for changed key

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,30 +1,22 @@
-import React, { Component } from "react";
-import Book from './Book.js'
-import "./App.css";
+import React, { Component } from 'react';
+import Book from './Book.js';
+import './App.css';
 
 //import {Route} from 'react-router-dom';
 
 class BookShelf extends Component {
-
-
-
-  
   render() {
-
     const { shelf, myBookList } = this.props;
-   
+
     return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{shelf.name}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            
-              {
-                myBookList.map( book => (
-                <Book key={book.key} book = {book} shelf = {shelf}/>
-                ))
-              }
-            
+      <div className='bookshelf'>
+        <h2 className='bookshelf-title'>{shelf.name}</h2>
+        <div className='bookshelf-books'>
+          <ol className='books-grid'>
+            {myBookList.map(book => (
+              <Book key={book.key} book={book} shelf={shelf} />
+              //book.industryIdentifiers[0].identifier
+            ))}
           </ol>
         </div>
       </div>
